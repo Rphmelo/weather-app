e6a09e8cfb375ee978e0aef5f703ee31e7147fe8
DIR\r.de.melo.silva <r.de.melo.silva@accenture.com>
2018-11-01 11:07:50 -0300

    add weather list component

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,8 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city, countryCode) {
     const url = `${ROOT_URL}&q=${city},${countryCode}`;
     const request = axios.get(url);
+
+    
     return {
         type: FETCH_WEATHER,
         payload: request
